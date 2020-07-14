b6bab6488f55042cff02c85ca230b20f24a612a2
郭世伟 <guoshiwei@bogon.tal-inc.com.cn>
2020-07-14 19:03:51 +0800

    docs(vuepress): 完善文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    base:"/",
+    title:"guo-ui",
+    description:"一个vue h5框架",
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+        ]
+    }
+}
